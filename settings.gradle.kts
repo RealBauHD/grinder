0e1351beb394480e81acc6824ae76cdbf8a90ed2
Aaron <bauhd@gmx.net>
2026-01-06 17:36:15 +0100

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,15 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenLocal()
+        maven("https://repo.papermc.io/repository/maven-public/")
+    }
+}
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.9.0"
+}
+
+rootProject.name = "grinder"
+
+include("grinder-api", "grinder-server")
